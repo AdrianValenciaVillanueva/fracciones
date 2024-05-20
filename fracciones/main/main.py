c3f1e9a11fe1d47d8013b105a86b0bff3684b765
AdrianValenciaVillanueva <157079095+AdrianValenciaVillanueva@users.noreply.github.com>
2024-05-20 12:41:24 -0600

    Add files via upload

--- fracciones/main/main.py
+++ fracciones/main/main.py
@@ -1,13 +1,15 @@
 import flet as ft
-
+#titulo de la pagina principal
 title =ft.Text(
     text_align=ft.TextAlign.CENTER,
     value=f"Fracciones",
     size=60,
     color = "black",
+    
 )
 
-def createContainer(symbol, on_click, color1, color2, page):
+#funcion para crear los contenedores de los botones
+def createContainer(symbol,route, color1, color2, page):
     outer_container = ft.Container(
         padding = 0,
         alignment = ft.alignment.bottom_center,
@@ -26,75 +28,114 @@ def createContainer(symbol, on_click, color1, color2, page):
         border_radius=10,
         content=ft.Text(value=symbol, color="white", size=50, text_align=ft.TextAlign.CENTER),
         ink=True,
-        on_click=lambda e: on_click(e, page)
+        on_click=lambda _: page.go("/"+route),
     )
 
     outer_container.content = inner_container
     return outer_container
 
-def on_suma_click(e, page: ft.Page):
-    # Limpiamos la página
-    page.clean()
-    # Agregamos el nuevo contenido
-    page.add(ft.Text(value="Estás en la ventana de suma", color="black", size=50, text_align=ft.TextAlign.CENTER))
-
-def on_resta_click(e, page: ft.Page):
-    # Limpiamos la página
-    page.clean()
-    # Agregamos el nuevo contenido
-    page.add(ft.Text(value="Estás en la ventana de resta", color="black", size=50, text_align=ft.TextAlign.CENTER))
-
-def on_multi_click(e, page: ft.Page):
-    # Limpiamos la página
-    page.clean()
-    # Agregamos el nuevo contenido
-    page.add(ft.Text(value="Estás en la ventana de multiplicacion", color="black", size=50, text_align=ft.TextAlign.CENTER))
-
-def on_divi_click(e, page: ft.Page):
-    # Limpiamos la página
-    page.clean()
-    # Agregamos el nuevo contenido
-    page.add(ft.Text(value="Estás en la ventana de divi", color="black", size=50, text_align=ft.TextAlign.CENTER))
-
-def on_informacion_click(e, page: ft.Page):
-    # Limpiamos la página
-    page.clean()
-    image_path = "C:\\Users\\Dario\\Desktop\\partes.png"
-    page.add(ft.Image(image_path, width=600, height=600))
-
 def main(page: ft.Page):
-    containerSuma = createContainer("+", on_suma_click, "#33915c", "#4bb36c", page)
-    containerResta = createContainer("-", on_resta_click, "#f8bc25", "#ffd633", page)
-    containerDivision = createContainer("x", on_multi_click, "#5c8ad6", "#5cb2e8", page)
-    containerMultiplicacion = createContainer("÷", on_divi_click, "#c20829", "#eb2b47", page)
-    containerInformacion = createContainer("?", on_informacion_click, "#a245f7", "#a552f2", page)
+    
+    page.title = "Routes Example"
+    containerSuma = createContainer("+","suma", "#33915c", "#4bb36c", page)
+    containerResta = createContainer("-", "resta", "#f8bc25", "#ffd633", page)
+    containerDivision = createContainer("x", "multiplicacion", "#5c8ad6", "#5cb2e8", page)
+    containerMultiplicacion = createContainer("÷", "division", "#c20829", "#eb2b47", page)
+    containerInformacion = createContainer("?", "ayuda", "#a245f7", "#a552f2", page)
 
-    page.adaptive = True
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    color = "#5271ff"
-    page.bgcolor = color
+    def route_change(route):
+    
+       
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                controls=[
+                    ft.Row(controls=[title],
+                           alignment=ft.MainAxisAlignment.CENTER,),
+                    ft.Column(controls=[
+                        ft.Row(controls=[
+                            containerSuma,
+                            containerResta,   
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        ),
+                        ft.Row(controls=[
+                            containerDivision,
+                            containerMultiplicacion,     
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER),
+                        ft.Row(controls=[
+                            containerInformacion,
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER)
 
-    page.add(
-        title,
-        ft.Column([
-            ft.Row(controls=[
-                containerSuma,
-                containerResta,   
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-            ),
-            ft.Row(controls=[
-                containerDivision,
-                containerMultiplicacion,     
-            ],
-            alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row(controls=[
-                containerInformacion,
-            ],
-            alignment=ft.MainAxisAlignment.CENTER)
+                    ],),
+                ],
+                vertical_alignment=ft.MainAxisAlignment.CENTER,
+                bgcolor="#5271ff",
+            )
+        )
+        if page.route == "/suma":
+            page.views.append(
+                ft.View(
+                    "/suma",
+                    [
+                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+        if page.route == "/resta":
+            page.views.append(
+                ft.View(
+                    "/resta",
+                    [
+                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+        if page.route == "/multiplicacion":
+            page.views.append(
+                ft.View(
+                    "/multiplicacion",
+                    [
+                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+        if page.route == "/division":
+            page.views.append(
+                ft.View(
+                    "/division",
+                    [
+                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+        if page.route == "/ayuda":
+            page.views.append(
+                ft.View(
+                    "/ayuda",
+                    [
+                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+        page.update()
 
-        ]),
-    )
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
 
-ft.app(main)
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
+
+ft.app(target=main)
